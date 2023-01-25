797657162da0877267f54ba5eda652c24187e985
Ahmed Ali <sucounix@gmail.com>
2023-01-25 10:35:20 +0200

    fargate-working

--- 02-flask-app/docker-build-push.sh
+++ 02-flask-app/docker-build-push.sh
@@ -2,12 +2,12 @@
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 086951686584.dkr.ecr.us-east-1.amazonaws.com
 
-echo "Falsk"
-docker build -t flask .
+# echo "Falsk"
+# docker build -t flask .
 
-docker tag flask:latest 086951686584.dkr.ecr.us-east-1.amazonaws.com/flask:latest
+# docker tag flask:latest 086951686584.dkr.ecr.us-east-1.amazonaws.com/flask:latest
 
-docker push 086951686584.dkr.ecr.us-east-1.amazonaws.com/flask:latest
+# docker push 086951686584.dkr.ecr.us-east-1.amazonaws.com/flask:latest
 
 echo "Nginx"
 
