f42a391dad363fcd8625fa361fdb8af2cb5057e8
Ahmed Ali <sucounix@gmail.com>
2023-01-28 18:21:12 +0200

    Next-CICD

---
+++ 01-infra/base/dev/terraform.tfvars
@@ -0,0 +1,10 @@
+# app/env to scaffold
+name = "xpov-dev"
+environment = "dev"
+region = "us-east-1"
+aws_profile = "default"
+saml_role = "xpov-admin"
+tags = {
+  name   = "xpov-flask-app"
+  environment   = "dev"
+}
--- 02-flask-app/docker-build-push.sh
+++ 02-flask-app/docker-build-push.sh
@@ -3,15 +3,15 @@
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 086951686584.dkr.ecr.eu-central-1.amazonaws.com
 
 echo "Falsk"
-docker build -t xpov-dev .
+docker build -t xpovx-dev .
 
-docker tag xpov-dev:latest 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpov-dev-falsk-app:latest
+docker tag xpovx-dev:test 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-falsk-app:latest
 
-docker push 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpov-dev-falsk-app:latest
+docker push 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-falsk-app:latest
 
 echo "Nginx"
 docker build -t nginx ./nginx
 
-docker tag nginx:latest 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpov-dev-nginx:latest
+docker tag nginx:test 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-nginx:latest
 
-docker push 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpov-dev-nginx:latest
+docker push 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-nginx:latest
