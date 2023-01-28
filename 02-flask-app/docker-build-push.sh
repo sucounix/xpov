56a082ed7dfdf1ac37b385b09e30a964e34dab19
Ahmed Ali <sucounix@gmail.com>
2023-01-28 13:56:13 +0200

    Morming_ECS

--- 02-flask-app/docker-build-push.sh
+++ 02-flask-app/docker-build-push.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 086951686584.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 086951686584.dkr.ecr.eu-central-1.amazonaws.com
 
-# echo "Falsk"
-# docker build -t flask .
+echo "Falsk"
+docker build -t xpov-dev .
 
-# docker tag flask:latest 086951686584.dkr.ecr.us-east-1.amazonaws.com/flask:latest
+docker tag xpov-dev:latest 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpov-dev-falsk-app:latest
 
-# docker push 086951686584.dkr.ecr.us-east-1.amazonaws.com/flask:latest
+docker push 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpov-dev-falsk-app:latest
 
 echo "Nginx"
-
 docker build -t nginx ./nginx
 
-docker tag nginx:latest 086951686584.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
+docker tag nginx:latest 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpov-dev-nginx:latest
 
-docker push 086951686584.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
+docker push 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpov-dev-nginx:latest
