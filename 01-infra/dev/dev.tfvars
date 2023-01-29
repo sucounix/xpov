f01d08a66bb8730f863f514b7d9612bc9e567342
Ahmed Ali <sucounix@gmail.com>
2023-01-29 09:00:49 +0200

    update

---
+++ 01-infra/dev/dev.tfvars
@@ -0,0 +1,12 @@
+name                = "xpovx"
+environment         = "dev"
+availability_zones  = ["eu-central-1a", "eu-central-1b"]
+private_subnets     = ["10.0.0.0/20", "10.0.32.0/20"]
+public_subnets      = ["10.0.16.0/20", "10.0.48.0/20"]
+tsl_certificate_arn = "arn:aws:acm:us-east-1:086951686584:certificate/445a4127-69fb-47a6-9545-c2c9d9b2e93a"
+container_memory_0    = 512
+container_memory_1    = 512
+container_name_0    = "nginx"
+container_image_0   = "nginx"
+container_name_1    = "falsk-app"
+container_image_1   = "falsk-app"
---
+++ 01-infra/dev/ecr/main.tf
@@ -0,0 +1,77 @@
+resource "aws_ecr_repository" "main" {
+  # for_each             = toset(module.this.enabled ? local.image_names : [])
+  # name                 = each.value
+
+  name                 = "${var.name}-${var.environment}-${var.container_name_0}"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+
+resource "aws_ecr_lifecycle_policy" "main" {
+# for_each   = toset(module.this.enabled && var.enable_lifecycle_policy ? local.image_names : [])
+# repository = aws_ecr_repository.name[each.value].name
+  repository = aws_ecr_repository.main.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "keep last 10 images"
+      action       = {
+        type = "expire"
+      }
+      selection     = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 10
+      }
+    }]
+  })
+}
+
+
+
+
+resource "aws_ecr_repository" "container_name_1" {
+
+  name                 = "${var.name}-${var.environment}-${var.container_name_1}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+
+resource "aws_ecr_lifecycle_policy" "container_name_1" {
+
+  repository = aws_ecr_repository.container_name_1.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "keep last 10 images"
+      action       = {
+        type = "expire"
+      }
+      selection     = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 10
+      }
+    }]
+  })
+}
+
+
+
+
+output "aws_ecr_repository_url" {
+    value = aws_ecr_repository.main.repository_url
+}
+
+output "aws_ecr_repository_url_container_name_1" {
+    value = aws_ecr_repository.container_name_1.repository_url
+}
