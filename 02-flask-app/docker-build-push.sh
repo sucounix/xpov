d40a635c05d91e7cb96dd11d900776780ca9732d
Ahmed Ali <sucounix@gmail.com>
2023-01-30 10:10:34 +0200

    update_port_8600

--- 02-flask-app/docker-build-push.sh
+++ 02-flask-app/docker-build-push.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+
+# ECR
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 086951686584.dkr.ecr.eu-central-1.amazonaws.com
 
 echo "Falsk"
@@ -15,3 +17,18 @@ docker build -t nginx ./nginx
 docker tag nginx:test 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-nginx:latest
 
 docker push 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-nginx:latest
+
+
+# # Local Repo.
+
+# echo "Falsk"
+# docker build -t xpovx-dev .
+
+# docker tag xpovx-dev-falsk-app:latest xpovx-dev-falsk-app:dev
+
+
+
+# echo "Nginx"
+# docker build -t nginx ./nginx
+
+# docker tag xpovx-dev-nginx:latest xpovx-dev-nginx:dev
