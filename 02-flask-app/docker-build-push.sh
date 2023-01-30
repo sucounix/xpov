41c224a3b6cb90cddbc4c82728068fe53bf0d8ea
Ahmed Ali <sucounix@gmail.com>
2023-01-30 11:18:50 +0200

    update

--- 02-flask-app/docker-build-push.sh
+++ 02-flask-app/docker-build-push.sh
@@ -7,14 +7,14 @@ aws ecr get-login-password --region eu-central-1 | docker login --username AWS -
 echo "Falsk"
 docker build -t xpovx-dev .
 
-docker tag xpovx-dev:test 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-falsk-app:latest
+docker tag xpovx-dev:latest 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-falsk-app:latest
 
 docker push 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-falsk-app:latest
 
 echo "Nginx"
 docker build -t nginx ./nginx
 
-docker tag nginx:test 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-nginx:latest
+docker tag nginx:latest 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-nginx:latest
 
 docker push 086951686584.dkr.ecr.eu-central-1.amazonaws.com/xpovx-dev-nginx:latest
 
